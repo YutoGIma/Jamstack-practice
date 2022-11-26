7d7b5b439f307196a344a3329417271572237e46
YutoGIma <yurihanagima@gmail.com>
2022-11-26 15:20:18 +0900

    jamstack-deploy

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,6 +1,6 @@
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-	serviceDomain: "sampleblog0824",
+	serviceDomain: "jamstackpractice",
 	apiKey: process.env.API_KEY,
 });
